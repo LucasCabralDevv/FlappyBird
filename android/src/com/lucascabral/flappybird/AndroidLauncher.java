a567243629ee331199eedc1ead36ebf88a7c85d0
lucascabraldeve <lucas_cabral.deve@outlook.com>
2020-11-26 16:49:21 -0300

    Initial commit

---
+++ android/src/com/lucascabral/flappybird/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.lucascabral.flappybird;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.lucascabral.flappybird.MyGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MyGame(), config);
+	}
+}
