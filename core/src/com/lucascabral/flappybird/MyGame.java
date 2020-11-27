f793e2a1b45a2c8afc100c7f9645a04cfbc48d1d
lucascabraldeve <lucas_cabral.deve@outlook.com>
2020-11-27 11:11:34 -0300

    Contabilização da pontuação finalizada.

--- core/src/com/lucascabral/flappybird/MyGame.java
+++ core/src/com/lucascabral/flappybird/MyGame.java
@@ -2,7 +2,9 @@ package com.lucascabral.flappybird;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 import java.util.Random;
@@ -26,6 +28,11 @@ public class MyGame extends ApplicationAdapter {
     private float posicaoYCano;
     private float espacoEntreCanos;
     private Random random;
+    private int pontuacao = 0;
+    private boolean passouCano = false;
+
+    // Exibição de textos
+    BitmapFont textoPontuacao;
 
     @Override
     public void create() {
@@ -38,6 +45,7 @@ public class MyGame extends ApplicationAdapter {
     public void render() {
 
         verificaEstadoJogo();
+        validarPontos();
         desenharTexturas();
     }
 
@@ -48,6 +56,7 @@ public class MyGame extends ApplicationAdapter {
         if (posicaoXCano < - canoBaixo.getWidth()){
             posicaoXCano = larguraDispositivo;
             posicaoYCano = random.nextInt(1400) - 700;
+            passouCano = false;
         }
 
         // Aplica evento de click na tela
@@ -74,10 +83,22 @@ public class MyGame extends ApplicationAdapter {
         batch.begin();
 
         batch.draw(fundo, 0, 0, larguraDispositivo, alturaDispositivo);
-        batch.draw(passaros[(int) variacao], 35, posicaoInicialYPassaro);
+        batch.draw(passaros[(int) variacao], 45, posicaoInicialYPassaro);
         batch.draw(canoBaixo, posicaoXCano, alturaDispositivo / 2 - canoBaixo.getHeight() - espacoEntreCanos / 2 + posicaoYCano);
         batch.draw(canoTopo, posicaoXCano, alturaDispositivo / 2 + espacoEntreCanos / 2 + posicaoYCano);
+        textoPontuacao.draw(batch, String.valueOf(pontuacao), larguraDispositivo/2, alturaDispositivo - 30);
         batch.end();
+    }
+
+    private void validarPontos (){
+
+        if (posicaoXCano < 45 - passaros[0].getWidth()){ // Cano já passou do pássaro
+
+            if (!passouCano){
+                pontuacao++;
+                passouCano = true;
+            }
+        }
     }
 
     private void inicializarTexturas() {
@@ -100,6 +121,11 @@ public class MyGame extends ApplicationAdapter {
         posicaoInicialYPassaro = alturaDispositivo / 2;
         posicaoXCano = larguraDispositivo;
         espacoEntreCanos = 300;
+
+        // Configura texto
+        textoPontuacao = new BitmapFont();
+        textoPontuacao.setColor(Color.WHITE);
+        textoPontuacao.getData().setScale(10);
     }
 
     @Override
