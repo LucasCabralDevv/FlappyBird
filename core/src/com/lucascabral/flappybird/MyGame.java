3870145066d4f5182831efa0750b11457eb1fa68
lucascabraldeve <lucas_cabral.deve@outlook.com>
2020-11-27 08:41:58 -0300

    Configurado a movimentação do passarro.

--- core/src/com/lucascabral/flappybird/MyGame.java
+++ core/src/com/lucascabral/flappybird/MyGame.java
@@ -7,27 +7,57 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class MyGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
+    private int movimentoX = 0;
+    private int movimentoY = 0;
+    private SpriteBatch batch;
+    private Texture[] passaros;
+    private Texture fundo;
+
+    //Atributos de configurações
+    private float larguraDispositivo;
+    private float alturaDispositivo;
+    private float variacao = 0;
+
+    @Override
+    public void create() {
+        //Gdx.app.log("create", "jogo iniciado");
+        batch = new SpriteBatch();
+        passaros = new Texture[3];
+        passaros[0] = new Texture("passaro1.png");
+        passaros[1] = new Texture("passaro2.png");
+        passaros[2] = new Texture("passaro3.png");
+        fundo = new Texture("fundo.png");
+
+        larguraDispositivo = Gdx.graphics.getWidth();
+        alturaDispositivo = Gdx.graphics.getHeight();
+    }
+
+    @Override
+    public void render() {
+
+        batch.begin();
+
+        if (variacao > 2)
+            variacao = 0;
+
+        batch.draw(fundo, 0, 0, larguraDispositivo, alturaDispositivo);
+        batch.draw(passaros[(int) variacao], 30, alturaDispositivo / 2);
+
+        variacao += Gdx.graphics.getDeltaTime() * 10;
+
+
+        movimentoX++;
+        movimentoY++;
+
+        batch.end();
+
+		/*contador++;
+		Gdx.app.log("render", "jogo renderizado: " + contador); */
+    }
+
+    @Override
+    public void dispose() {
+        //Gdx.app.log("dispose", "descarte de conteúdos");
+    }
 }
